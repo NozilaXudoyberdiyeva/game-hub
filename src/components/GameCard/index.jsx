14811d80ecda60cb6c953a37baf1e6f6ae2c7960
Your Name <you@example.com>
2023-12-02 12:02:09 +0500

    buil get cropped image

--- src/components/GameCard/index.jsx
+++ src/components/GameCard/index.jsx
@@ -1,9 +1,10 @@
 import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
 import PropTypes from "prop-types";
+import getCroppedImage from "../../services/image-url";
 const GameCard = ({ game }) => {
   return (
     <Card borderRadius="xl" overflow="hidden">
-      <Image src={game.background_image} alt={game.name} />
+      <Image src={getCroppedImage(game.background_image)} alt={game.name} />
       <CardBody>
         <Heading size="lg">{game.name}</Heading>
       </CardBody>
